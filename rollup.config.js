107c9d84b7b41b04668332e21ac21e62d3332453
synw <synwx@protonmail.com>
2023-09-30 14:23:56 +0200

    Version 0.3.0: drop commonjs support

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 //const isProduction = !process.env.ROLLUP_WATCH;
 
 export default {
   input: 'src/main.ts',
   output: [
-    {
-      file: 'dist/api.cjs.min.js',
-      format: 'cjs',
-      plugins: [terser(),]
-    },
     {
       file: 'dist/api.es.js',
       format: 'esm'
@@ -30,7 +24,6 @@ export default {
       main: true,
       browser: true,
     }),
-    commonjs(),
     //isProduction && terser({ format: { comments: false } }),
   ],
 };
