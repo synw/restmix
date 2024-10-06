50500321194cbeb7f865e65e746eef6ced48d692
synw <synwe@yahoo.com>
2024-10-06 15:29:11 +0200

    Added new functionality to test suite
    - Added support for onResponse hook in api
    - Added tests for patch endpoint and non-existent resource
    - Added tests for addHeader, removeHeader, setCsrfToken methods
    - Updated import statement to include OnResponseHook interface
    - Modified existing tests to use new functionality

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,5 @@
 import { useApi } from '../src/api';
+import { OnResponseHook } from '../src/interfaces';
 
 const api = useApi({
   serverUrl: 'http://localhost:5714',
@@ -33,9 +34,35 @@ describe('tests', () => {
     const res = await api.put<Record<string, any>>("/put", payload);
     expect(res.data).toEqual({ response: "ok" })
   });
-  /*it('patch', async () => {
+  it('patch', async () => {
     const payload = { foo: "bar" };
-    const res = await api.patch<Record<string, any>>("/patch", payload, true);
-    expect(res).toEqual({ response: "ok" })
+    const res = await api.patch<Record<string, any>>("/patch", payload);
+    expect(res.data).toEqual({})
+  });
+  it('patch non-existent resource', async () => {
+    const payload = { foo: "bar" };
+    const res = await api.patch<Record<string, any>>("/non-existent", payload);
+    expect(res.status).toEqual(400)
+  });
+  /*it('addHeader and removeHeader', async () => {
+    api.addHeader('X-Custom-Header', 'custom-value');
+    const res = await api.get<Record<string, any>>("/");
+    expect(res.headers['X-Custom-header']).toEqual('custom-value');
+    api.removeHeader('X-Custom-Header');
+    const res2 = await api.get<Record<string, any>>("/");
+    expect(res2.headers['X-Custom-header']).toBeUndefined();
   });*/
+  it('onResponse hook', async () => {
+    const hook: OnResponseHook = (res) => {
+      res.data = { ...res.data, modified: true };
+      return Promise.resolve(res);
+    };
+    api.onResponse(hook);
+    const res = await api.get<Record<string, any>>("/");
+    expect(res.data).toEqual({ response: "ok", modified: true });
+  });
+  it('setCsrfToken', async () => {
+    api.setCsrfToken('test-csrf-token');
+    expect(api.csrfToken()).toEqual('test-csrf-token');
+  });
 });
