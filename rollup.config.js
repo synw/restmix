ef92b341960aef286c1ed8757c6a3547dcf7e84c
synw <synwe@yahoo.com>
2025-11-26 14:32:47 +0100

    Update package version and build configuration to use main.js instead of api.es.js
    - Bump version from 0.6.0 to 0.6.1 in package.json
    - Update module entry point from ./dist/api.es.js to ./dist/main.js in package.json
    - Update exports configuration to use ./dist/main.js instead of ./dist/api.es.js
    - Rename output files in rollup.config.js from api.es.js and api.min.js to main.js and main.min.js
    - Change import statement in src/main.ts to use import type for interface imports

--- rollup.config.js
+++ rollup.config.js
@@ -8,11 +8,11 @@ export default {
   input: 'src/main.ts',
   output: [
     {
-      file: 'dist/api.es.js',
+      file: 'dist/main.js',
       format: 'esm'
     },
     {
-      file: 'dist/api.min.js',
+      file: 'dist/main.min.js',
       format: 'iife',
       name: '$api',
       plugins: [terser()]
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { useApi } from "./api.js";
-import { UseApiParams, ApiResponse, OnResponseHook } from "./interfaces.js";
+import type { UseApiParams, ApiResponse, OnResponseHook } from "./interfaces.js";
 
 export { useApi, UseApiParams, ApiResponse, OnResponseHook };
